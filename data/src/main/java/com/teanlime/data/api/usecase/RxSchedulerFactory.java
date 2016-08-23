324a9db59c72f3d0804594d829ff8d1b87f4cf96
tea.n.lime <tea.n.lime@gmail.com>
2016-08-23 20:15:10 +0100

    #96 Cover code in unit tests
    Added documentation for RxSchedulerFactory

--- data/src/main/java/com/teanlime/data/api/usecase/RxSchedulerFactory.java
+++ data/src/main/java/com/teanlime/data/api/usecase/RxSchedulerFactory.java
@@ -2,9 +2,24 @@ package com.teanlime.data.api.usecase;
 
 import rx.Scheduler;
 
+/**
+ * Provides instances of relevant execution schedulers for the purpose of RXJava operations
+ * Each scheduler can be it's own thread
+ */
 public interface RxSchedulerFactory {
 
+    /**
+     * Provides an execution scheduler, which is the scheduler and thread the RX operations will be performed on
+     *
+     * @return execution scheduler
+     */
     Scheduler getExecutionScheduler();
 
+    /**
+     * Provides a post-execution scheduler, which is the scheduler and thread the result of the RX operations will be
+     * pushed out on
+     *
+     * @return post-execution scheduler
+     */
     Scheduler getPostExecutionScheduler();
 }
